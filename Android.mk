c6d31dd8d91b3bb8a846bea630bcd02cae3660c4
Alexander Pohl <pohl199885@gmail.com>
2016-10-16 16:55:34 +0200

    vendor_oneplus: oneplusX/6.0.1: add CNEService and com.qualcomm.location from latest mm oos

--- Android.mk
+++ Android.mk
@@ -156,6 +156,28 @@ LOCAL_SRC_FILES     := priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
 LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/priv-app
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE        := CNEService
+LOCAL_MODULE_OWNER  := qcom
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := APPS
+LOCAL_CERTIFICATE   := platform
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_SRC_FILES     := priv-app/CNEService/CNEService.apk
+LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/priv-app
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := com.qualcomm.location
+LOCAL_MODULE_OWNER  := qcom
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := APPS
+LOCAL_CERTIFICATE   := platform
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_SRC_FILES     := priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/priv-app
+include $(BUILD_PREBUILT)
+
 $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wcd9320; \
         ln -sf /data/misc/audio/wcd9320_anc.bin \
                 $(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_anc.bin;\
